3abbb04f5923fb997f1c966e19ffee164a4f63a2
dhacha-andika <dhachaandika95@gmail.com>
2024-09-06 18:31:42 +0700

    search iphone in digimap

--- cypress/e2e/test-1.cy.js
+++ cypress/e2e/test-1.cy.js
@@ -1,17 +1,27 @@
-import { faker } from '@faker-js/faker';
-describe('change language', () => {
-  it('iphone on amazon', () => {
-    cy.visit('https://www.lazada.co.id/#?');
-    cy.get('#q').click().type("Phone 15 Pro");
+describe('My First Test', () => {
+  it('Searching Iphone', () => {
+    
+    const products = []
+    
+    cy.visit('https://www.digimap.co.id');
+
+    cy.get('[class="icon icon-hamburger"]').click();
     Cypress.on('uncaught:exception', (err, runnable) => {
       return false;
     });
-    
-    
-
-
-
-
-  });
- 
+    cy.get('input[name="q"]').eq(0).click().type('iphone 15{enter}');
+    cy.get('body').contains('iphone 15').should('be.visible');
+    cy.get('h3').contains('iPhone 15 512GB Pink').should('be.visible').click();
+    cy.get('product-info').should('be.exist');
+    cy.get('product-info').each(($el) => {
+      const name = $el.find('.product-title-style product__title').text()
+      const price = parseFloat($el.find('.js-product-price-with-care-warranty actual_price_bold').text().replace(/[^0-9.]/g, '')) // Convert price to number
+      products.push({ name, price });
+      cy.log(`Name: ${name}`)
+      cy.log(`Price: ${price}`)
+    });
+   
+   //
+  })
+  
 })
